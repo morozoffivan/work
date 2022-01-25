729e2c41120e44ec2b684335f1c652667ca7b824
ivan <ivan.morozov.nrj@yandex.ru>
2022-01-25 20:10:51 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// // const result = confirm("Are you here?");
+// // console.log(result);
+
+// // const answere = +prompt ("Вам есть 18?", "18");
+// // console.log(answere + 5);
+
+// const answers = [];
+
+// answers[0] = prompt("Имя?", "");
+// answers[1] = prompt("Фамилия?", "");
+// answers[2] = prompt("Лет?", "");
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+const category = "toys";
+console.log(`https://someurl.com/${category}/5`);
+
+const user = "Ivan";
+
+alert(`Привет, ${user}`);
